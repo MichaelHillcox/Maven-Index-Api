068b79f734c7ddc9d7d6a54a605a8667e6badd89
Michael Hillcox <michaelhillcox@gmail.com>
2021-05-31 12:19:35 +0100

    feat: pagination, cleaned routes, files, more

--- src/main/java/pro/mikey/MavenApi.java
+++ src/main/java/pro/mikey/MavenApi.java
@@ -1,21 +1,16 @@
 package pro.mikey;
 
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.javalin.Javalin;
 import io.javalin.plugin.json.JavalinJackson;
 
-import java.io.File;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Instant;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MavenApi {
     private static final MetaMavenFetcher MAVEN_FETCHER = new MetaMavenFetcher();
@@ -26,36 +21,76 @@ public class MavenApi {
 
         JavalinJackson.configure(new ObjectMapper().registerModule(new JavaTimeModule()));
 
-        app.get("/", ctx -> {
+        app.get("/repos", ctx -> {
             HashSet<MavenMeta> repos = MAVEN_FETCHER.getRepos(mavenRoot);
-            ctx.json(repos);
+
+            ctx.json(repos.stream().map(MavenMeta::groupId).distinct().collect(Collectors.toList()));
         });
 
-        app.get("/repo/:name/files", ctx -> {
+        app.get("/repos/:group/projects", ctx -> {
             HashSet<MavenMeta> repos = MAVEN_FETCHER.getRepos(mavenRoot);
-            Optional<MavenMeta> name = repos.stream().filter(e -> e.artifact().equals(ctx.pathParam("name"))).findFirst();
+            String group = ctx.pathParam("group");
+            if (repos.stream().noneMatch(e -> e.groupId().equals(group))) {
+                ctx.status(404).json(new ResError("Not found"));
+                return;
+            }
 
-            name.ifPresentOrElse((e) -> {
+            ctx.json(repos.stream().filter(e -> e.groupId().equals(group)).map(MavenMeta::artifact).collect(Collectors.toList()));
+        });
+
+        app.get("/repo/:group/:artifact", ctx -> {
+            findByIdentifier(mavenRoot, ctx.pathParam("group"), ctx.pathParam("artifact"))
+                .ifPresentOrElse(ctx::json, () -> ctx.status(404).json(new ResError("Not found")));
+        });
+        
+        app.get("/repo/:group/:artifact/files", ctx -> {
+            findByIdentifier(mavenRoot, ctx.pathParam("group"), ctx.pathParam("artifact")).ifPresentOrElse((e) -> {
+                String pageParam = ctx.queryParam("page");
+
+                // Pagination
+                int page = pageParam == null
+                    ? 1
+                    : Math.max(1, Integer.parseInt(pageParam));
+
+                int results = e.versions().length;
+                int limit = 50;
+                int offset = (page - 1) * limit;
+                int remaining = Math.max(0, results - (page * limit));
+
                 HashMap<String, String[]> files = new HashMap<>();
-                for (String version : e.versions()) {
-                    files.put(version, e.home().resolve(version).toFile().list());
+                for (int i = offset; i < Math.min(page * limit, results); i++) {
+                    files.put(e.versions()[i], e.home().resolve(e.versions()[i]).toFile().list());
                 }
 
-                ctx.json(new MavenFiles(
-                    e.latest(),
-                    e.release(),
-                    e.updated(),
-                    files
-                ));
+                ctx.json(new Pagination<>(e, files, new PageInfo(page, results, limit, offset, remaining)));
             }, () -> ctx.status(404).json(new ResError("Nothing found")));
         });
     }
 
-    static record ResError(String message) {}
-    static record MavenFiles(
-        String latest,
-        String release,
-        Instant updated,
-        HashMap<String, String[]> files
-    ) {}
+    private static Optional<MavenMeta> findByIdentifier(Path path, String group, String artifact) {
+        HashSet<MavenMeta> repos = MAVEN_FETCHER.getRepos(path);
+
+        return repos.stream()
+            .filter(e -> e.groupId().equals(group) && e.artifact().equals(artifact))
+            .findFirst();
+    }
+
+    static record ResError(String message) {
+    }
+
+    static record Pagination<K, T>(
+        K meta,
+        T items,
+        PageInfo page
+    ) {
+    }
+
+    static record PageInfo(
+        int page,
+        int results,
+        int limit,
+        int offset,
+        int remaining
+    ) {
+    }
 }
--- src/main/java/pro/mikey/MavenMeta.java
+++ src/main/java/pro/mikey/MavenMeta.java
@@ -1,13 +1,16 @@
 package pro.mikey;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.StringJoiner;
 
 @JsonSerialize
+@JsonIgnoreProperties({"home"})
 public record MavenMeta(
         Path home,
         String artifact,
@@ -17,11 +20,16 @@ public record MavenMeta(
         Instant updated,
         String[] versions
 ) {
+    @JsonSerialize
+    public String location() {
+        return this.groupId.replace(".", "/") + "/" + this.artifact;
+    }
 
     @Override
     public String toString() {
         return new StringJoiner(", ", MavenMeta.class.getSimpleName() + "[", "]")
             .add("home=" + home)
+            .add("location=" + location())
             .add("artifact='" + artifact + "'")
             .add("groupId='" + groupId + "'")
             .add("release='" + release + "'")
